daec23a8ac445955f23650e68b97954cffb04b2b
j8won <joowoni99@gmail.com>
2022-11-22 23:11:23 +0900

    chore: 라우터 및 스타일 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider as StyledThemeProvider, createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+import Theme from "./styles/Theme";
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<div>Hi</div>} />
+      </Routes>
+    </Router>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <AppRouter />
+    </StyledThemeProvider>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  
+  * {
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    text-decoration: none;
+    outline: none;
+  }
+  body {
+    font-family: 'Apple SD Gothic Neo', sans-serif;
+    font-weight: 400;
+  }
+`;
 
 export default App;
---
+++ src/styles/Theme.js
@@ -0,0 +1,13 @@
+const Theme = {
+  orange: "#ffa77b",
+  yellow: "#ffd878",
+  green: "#0aae7a",
+
+  white: "#ffffff",
+  black: "#000000",
+
+  firstGray: "#333333",
+  secondGray: "#646464",
+}
+
+export default Theme;
