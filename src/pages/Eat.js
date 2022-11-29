315535795211a062c0b3bbde2b837794a8ca8153
j8won <joowoni99@gmail.com>
2022-11-29 10:55:22 +0900

    feat(Eat): EatInfo 스타일링

--- src/pages/Eat.js
+++ src/pages/Eat.js
@@ -1,11 +1,20 @@
 import styled from "styled-components";
 
+import plusIcon from '../images/Icon feather-plus-circle.webp'
+import EatInfo from "../components/eat/EatInfo";
+
 function Eat(){
   return (
     <Body>
       <Main>
         <div>메뉴 바</div>
-        <div>밥 먹어요 컴포넌트</div>
+        <Section>
+          <TitleBox>
+            <div className="title">밥먹어요</div>
+            <img className="plus-icon" src={plusIcon} />
+          </TitleBox>
+          <EatInfo />
+        </Section>
       </Main>
       <div>동방 현황바 컴포넌트</div>
     </Body>
@@ -17,17 +26,46 @@ const Body = styled.div`
   height: 100vh;
   
   display: grid;
-  grid-template-columns: auto 470px;
+  grid-template-columns: auto 25%;
   
   font-family: ${props => props.theme.logoFont};
 `
 
 const Main = styled.div`
-  display: flex;
-  flex-direction: column;
-  
   display: grid;
   grid-template-rows: 120px auto;
+`;
+
+const Section = styled.div`
+  padding: 7.5%;
+  background-color: ${props => props.theme.green};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleBox = styled.div`
+  width: 100%;
+  padding-bottom: 25px;
+  
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  
+  color: ${props => props.theme.brightColor};
+  font-family: ${props => props.theme.logoFont};
+  font-size: ${props => props.theme.titleFontSize};
+  
+  .title {
+    padding-right: 9px;
+  }
+  
+  .plus-icon {
+    width: 20px;
+    height: 20px;
+    object-fit: cover;
+  }
 `;
 
 export default Eat;
